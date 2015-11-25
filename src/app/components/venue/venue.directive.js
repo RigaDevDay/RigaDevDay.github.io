c2f72f99370b05391e59351fb438236d42fa0465
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2015-11-25 19:45:35 +0200

    Add hotel markes. Add popups for all markers

--- src/app/components/venue/venue.directive.js
+++ src/app/components/venue/venue.directive.js
@@ -18,7 +18,7 @@ class VenueController {
 
     this.map = {
       center: {latitude: 56.946333, longitude: 24.116870},
-      zoom: 16,
+      zoom: 15,
       options: {
         zoomControlOptions: {
           position: google.maps.ControlPosition.LEFT_CENTER
@@ -45,6 +45,28 @@ class VenueController {
           icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
           labelContent: 'Kino Citadele',
           labelAnchor: "46 -5",
+          labelClass: 'map-label'
+        }
+      },
+      avalon: {
+        idKey: 'avalon',
+        position: {latitude: 56.945492, longitude: 24.112021},
+        options: {
+          title: 'Avalon Hotel Riga',
+          icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+          labelContent: 'Avalon Hotel Riga',
+          labelAnchor: "60 -5",
+          labelClass: 'map-label'
+        }
+      },
+      mercure: {
+        idKey: 'mercure',
+        position: {latitude: 56.947525, longitude: 24.123884},
+        options: {
+          title: 'Mercure Riga Centre hotel',
+          icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+          labelContent: 'Mercure Riga Centre hotel',
+          labelAnchor: "86 -5",
           labelClass: 'map-label'
         }
       }
