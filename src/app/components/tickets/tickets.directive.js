915728137730ed7578a316062372b0430be2cd4c
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2015-11-29 23:02:02 +0200

    tickets improvement

--- src/app/components/tickets/tickets.directive.js
+++ src/app/components/tickets/tickets.directive.js
@@ -20,31 +20,31 @@ class TicketsController {
       {
         title: 'Early bird',
         time: '(01.10.15-30.11.15)',
-        ends: this.endsIn("30.11.15"),
+        ends: this.endsIn("1.12.15"),
         price: {day1: '89€+VAT', day2: '158€+VAT'},
         active: this.isInTimeRange('01.10.15', '30.11.15'),
         reserveLink: 'http://www.kalevatravel.lv/celojumi/page.php?id=667'
       },
       {
         title: 'Normal',
-        time: '(30.11.15-15.02.16)',
-        ends: this.endsIn("15.02.16"),
+        time: '(01.12.15-15.02.16)',
+        ends: this.endsIn("16.02.16"),
         price: {day1: '119€+VAT', day2: '198€+VAT'},
-        active: this.isInTimeRange('30.11.15', '15.02.16'),
+        active: this.isInTimeRange('01.12.15', '15.02.16'),
         reserveLink: 'http://www.kalevatravel.lv/celojumi/page.php?id=667'
       },
       {
         title: 'Late Bird',
-        time: '(15.02.16-03.03.16)',
-        ends: this.endsIn("03.03.16"),
+        time: '(16.02.16-03.03.16)',
+        ends: this.endsIn("04.03.16"),
         price: {day1: '150€+VAT', day2: '260€+VAT'},
-        active: this.isInTimeRange('15.02.16', '03.03.16'),
+        active: this.isInTimeRange('16.02.16', '03.03.16'),
         reserveLink: 'http://www.kalevatravel.lv/celojumi/page.php?id=667'
       },
       {
         title: 'Workshops',
         time: '(01.10.15-15.02.16)',
-        ends: this.endsIn("15.02.16"),
+        ends: this.endsIn("16.02.16"),
         price: false,
         active: false,
         reserveLink: ''
@@ -57,8 +57,8 @@ class TicketsController {
   }
 
   isInTimeRange (startDate, endDate) {
-    let startTime = this.moment(startDate, 'DD.MM.YYYY');
-    let endTime = this.moment(endDate, 'DD.MM.YYYY');
+    let startTime = this.moment(startDate, 'DD.MM.YYYY').startOf('day');
+    let endTime = this.moment(endDate, 'DD.MM.YYYY').endOf('day');
 
     let now = this.moment();
 
