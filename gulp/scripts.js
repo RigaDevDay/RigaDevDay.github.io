e49a9a962f5483009a016941533e48e9a52d762a
Dmitrijs Vrublevskis <d.vrublevskis@gmail.com>
2015-12-20 13:12:02 +0200

    Autogenerate main.json

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -4,27 +4,32 @@ var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
 
+var babel = require('babel-core');
+var vm = require('vm');
+var util = require('util');
+var fs = require('fs');
+
 var browserSync = require('browser-sync');
 var webpack = require('webpack-stream');
 
 var $ = require('gulp-load-plugins')();
 
-function webpackWrapper(watch, test, callback) {
+function webpackWrapper (watch, test, callback) {
   var webpackOptions = {
     watch: watch,
     module: {
-      preLoaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'}],
-      loaders: [{ test: /\.js$/, exclude: /node_modules/, loaders: ['ng-annotate', 'babel-loader']}]
+      preLoaders: [{test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'}],
+      loaders: [{test: /\.js$/, exclude: /node_modules/, loaders: ['ng-annotate', 'babel-loader']}]
     },
-    output: { filename: 'index.module.js' }
+    output: {filename: 'index.module.js'}
   };
 
-  if(watch) {
+  if (watch) {
     webpackOptions.devtool = 'inline-source-map';
   }
 
-  var webpackChangeHandler = function(err, stats) {
-    if(err) {
+  var webpackChangeHandler = function (err, stats) {
+    if (err) {
       conf.errorHandler('Webpack')(err);
     }
     $.util.log(stats.toString({
@@ -34,13 +39,13 @@ function webpackWrapper(watch, test, callback) {
       version: false
     }));
     browserSync.reload();
-    if(watch) {
+    if (watch) {
       watch = false;
       callback();
     }
   };
 
-  var sources = [ path.join(conf.paths.src, '/app/index.module.js') ];
+  var sources = [path.join(conf.paths.src, '/app/index.module.js')];
   if (test) {
     sources.push(path.join(conf.paths.src, '/app/**/*.spec.js'));
   }
@@ -52,6 +57,27 @@ function webpackWrapper(watch, test, callback) {
 
 gulp.task('scripts', function () {
   return webpackWrapper(false, false);
+});
+
+gulp.task('data', function () {
+  var code = babel.transformFileSync(
+    path.join(conf.paths.src, "/app/services/main.data.js")).code;
+
+  // cut out transpiler stuff
+  code = code.substring(79, code.length - 28);
+
+  var sandbox = {};
+  var context = new vm.createContext(sandbox);
+  var script = new vm.Script(code);
+  script.runInContext(context);
+
+  var json = JSON.stringify(sandbox.mainData);
+
+  fs.writeFile(path.join(conf.paths.src, '/assets/data/main.json'), json, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+  });
 });
 
 gulp.task('scripts:watch', ['scripts'], function (callback) {
